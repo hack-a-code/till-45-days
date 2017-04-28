a60ee8e0f0f720a0aca32bd81296ee45d2327af1
ASKRajatKaushal <askrajatkaushal@gmail.com>
2017-04-28 10:01:39 +0530

    updated jsons & index.js

--- rk/parser/index.js
+++ rk/parser/index.js
@@ -1,29 +1,36 @@
 const fs = require('fs');
 
 var readFiles = function (dirname, callbackOnFileContent, callbackOnError) {
-    fs.readdir(dirname, function(err, filenames) {
-        if (err) {
-            callbackOnError(err);
-            return;
-        }
-        filenames.forEach(function(filename) {
-            fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+    // fs.readdir(dirname, function(err, filenames) {
+    //     if (err) {
+    //         callbackOnError(err);
+    //         return;
+    //     }
+    //     filenames.forEach(function(filename) {
+    var filename = 'User.json';
+            fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
                 if (err) {
                     callbackOnError(err);
                     return;
                 }
                 callbackOnFileContent(filename, content);
             });
-        });
-    });
+    //     });
+    // });
 }
 
 var onFileContent = function (filename, content) {
-    console.log('File ', filename, ' contains : ', content);
+    content = JSON.parse(content);
+    Object.keys(content.paths).forEach( path => {
+        console.log('File ', filename, ' contains : ', path, ' route');
+        Object.keys(content.paths[path]).forEach( methodType => {
+            console.log('Above path contains : ', methodType, ' method');
+        });
+    });
 }
 
 var onError = function (error) {
     console.error('Following error occurred : ' + error);
 }
 
-readFiles('jsons/', onFileContent, onError);
+readFiles('jsons', onFileContent, onError);
